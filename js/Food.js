34adc26d87de383761b2be072cad1e3560baee62
payelbera <60781680+payelbera@users.noreply.github.com>
2020-11-06 22:49:06 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,14 @@
+class Food{
+
+    constructor(){
+        //  loadImage("images/Milk.png") in this.milkImg ;
+        this.foodStock =0;
+        this.lastFed=null;
+    }
+    getFoodStock(){}
+    updateFoodStock(){}
+    deductFood(){}
+    display(){
+        // look at the hint in pdf and write the func
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,81 @@
+//Create variables here
+var dog, dogImg,happyDogImg, database, foodS, foodStock;
+var feedBtn, addFoodBtn,fedTime, lastFed,foodObj // VP2
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("images/dogImg.png")
+  happyDogImg = loadImage("images/dogImg1.png")
+
+  
+}
+
+function setup() {
+  createCanvas(600, 500);
+  database = firebase.database();
+  dog = createSprite(250,250)
+  dog.addImage("dog",dogImg)
+  dog.addImage("doghappy",happyDogImg)
+  dog.scale = 0.2;
+  getFoodStock();
+  
+
+ 
+  //create feedBtn and addFoodBtn and position them on the screen
+  
+  //create new Food Object called foodObj
+  
+}
+
+
+function draw() {  
+
+  background(46, 139, 87);
+  fill("red")
+  stroke("white")
+  text("Press UP ARROW TO FEED DOG", 150, 50);
+  
+  
+  foodObj.foodStock = foodS;
+  //display foodObj
+
+  // read the value of FeedTime from DB and set up an .on listener
+    
+  // store the data in var lastFed = data.val();
+    
+  //call the showTime func and pass lastFed as parameter
+  
+  // call addFoodBtn 's mousePressed and call getFoodStock() and addFood(foodS);;
+    
+  
+  //call feedBtn's mousePressed function and call getFoodStock(); and feedDog();
+  
+  drawSprites();
+  
+
+}
+
+
+function showTime(time){
+//Look at the hint and fill the code for the func
+}
+
+function addFood(f){
+  f++;
+  database.ref('/').update({
+    Food : f
+  })
+}
+function getFoodStock(){
+  database.ref("Food").on("value",function(data){
+    foodS = data.val();
+  })
+}
+function feedDog(){
+  foodS--
+  database.ref("/").update({
+    FeedTime:hour(),
+    Food:foodS
+  })
+}
